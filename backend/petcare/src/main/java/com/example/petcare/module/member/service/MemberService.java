fa428d2d69b7ed27796bb78b394ee235362c52ed
진주원 <84346055+jinjoo-lab@users.noreply.github.com>
2025-02-12 11:49:21 +0900

    [FEAT] Member Response DTO 정의

--- backend/petcare/src/main/java/com/example/petcare/module/member/service/MemberService.java
+++ backend/petcare/src/main/java/com/example/petcare/module/member/service/MemberService.java
@@ -1,6 +1,9 @@
 package com.example.petcare.module.member.service;
 
+import com.example.petcare.module.member.dto.response.MemberResponse;
+import com.example.petcare.module.member.entity.Member;
 import com.example.petcare.module.member.repository.MemberRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,6 +12,11 @@ public class MemberService {
 
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
+    }
+
+    public MemberResponse getMemberById(Long id) {
+        Member member = memberRepository.findById(id).orElseThrow(EntityNotFoundException::new);
+
     }
 
 
