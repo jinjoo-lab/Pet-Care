94948a1c514fd37c02298793570d3231b53684e2
진주원 <84346055+jinjoo-lab@users.noreply.github.com>
2025-02-12 10:43:16 +0900

    [FEAT] Member Controller API 정의

---
+++ backend/petcare/src/main/java/com/example/petcare/module/member/service/MemberService.java
@@ -0,0 +1,15 @@
+package com.example.petcare.module.member.service;
+
+import com.example.petcare.module.member.repository.MemberRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+
+}
