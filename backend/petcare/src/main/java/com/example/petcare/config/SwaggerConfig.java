04b33289dc63c76ab351c6214ba0ae9b7e3bdc4a
진주원 <84346055+jinjoo-lab@users.noreply.github.com>
2025-02-13 16:54:28 +0900

    [FEAT] SwaggerConfig 위치 조정

--- backend/petcare/src/main/java/com/example/petcare/config/SwaggerConfig.java
+++ backend/petcare/src/main/java/com/example/petcare/config/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.example.petcare.global.audit;
+package com.example.petcare.config;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.OpenAPI;
 import lombok.RequiredArgsConstructor;
