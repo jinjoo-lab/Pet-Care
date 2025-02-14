118fc239410a43fed7ab6de5e4c3d1095034b99a
진주원 <84346055+jinjoo-lab@users.noreply.github.com>
2025-02-14 11:13:28 +0900

    [FIX] Species Entity 정의

--- backend/petcare/src/main/java/com/example/petcare/module/petsitter/entity/Species.java
+++ backend/petcare/src/main/java/com/example/petcare/module/petsitter/entity/Species.java
@@ -1,5 +1,29 @@
 package com.example.petcare.module.petsitter.entity;
 
-public enum Species {
-    DOG, CAT, BIRD, REPTILE, OTHER
+import com.example.petcare.global.audit.AuditListener;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLRestriction;
+
+@Entity
+@Getter
+@SQLRestriction("deleted_at IS NULL")
+@EntityListeners(AuditListener.class)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Species {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String topSpecies;
+
+    private String subSpecies;
+
+    public Species(String topSpecies, String subSpecies) {
+        this.topSpecies = topSpecies;
+        this.subSpecies = subSpecies;
+    }
+
 }
